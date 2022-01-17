6a8b532917a75bf8e6b693989fa0af1957b8f9e7
Aydin Alexis CEKEREK <alexis.cekerek@gmail.com>
2022-01-17 11:25:56 +0100

    first commit

---
+++ migrations/Version20220117100411.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220117100411 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE article ADD category_id INT DEFAULT NULL, ADD writer_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E661BC7E6B6 FOREIGN KEY (writer_id) REFERENCES writer (id)');
+        $this->addSql('CREATE INDEX IDX_23A0E6612469DE2 ON article (category_id)');
+        $this->addSql('CREATE INDEX IDX_23A0E661BC7E6B6 ON article (writer_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E6612469DE2');
+        $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E661BC7E6B6');
+        $this->addSql('DROP INDEX IDX_23A0E6612469DE2 ON article');
+        $this->addSql('DROP INDEX IDX_23A0E661BC7E6B6 ON article');
+        $this->addSql('ALTER TABLE article DROP category_id, DROP writer_id');
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        for ($i = 0; $i < 10; $i++)
+            $category = new Category();
+
+            $category->setName($faker->word);
+            $category->setDescription('$faker->text');
+
+            $manager->persist($category);
+
+        $manager->flush();
+    }
+}
